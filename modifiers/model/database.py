4f4ac097b07f29b4083c3958d3dc21fe2e56f7bb
xunto <hunterdelyx1@gmail.com>
2018-02-01 03:41:08 +0300

    Fix tests can't run without database

--- modifiers/model/database.py
+++ modifiers/model/database.py
@@ -5,17 +5,26 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from modifiers.model import schema
 
-client = AsyncIOMotorClient()
-collection: AgnosticCollection = client.modifiers.collection
-collection.create_index("username")
+__collection = None
+
+
+async def get_db() -> AgnosticCollection:
+    global __collection
+    if not __collection:
+        client = AsyncIOMotorClient()
+        __collection = client.modifiers.collection
+        __collection.create_index("username")
+    return __collection
 
 
 async def get_character(username: str) -> Optional[schema.Character]:
+    collection = await get_db()
     data = await collection.find_one({"username": username})
     return schema.Character(**data) if data else None
 
 
 async def save_character(character: schema.Character):
+    collection = await get_db()
     await collection.update_one(
         {"username": character.username},
         {"$set": character.dict()},
